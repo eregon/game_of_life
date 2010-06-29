b8f27128101729436b986c4dde0d3d6bc983b93b
eregon <eregontp@gmail.com>
2010-06-29 18:18:07 +0300

    Improve life_shoes

--- life_shoes.rb
+++ life_shoes.rb
@@ -13,21 +13,21 @@ Shoes.app :title => 'Game of Life'do
   def initialize_cells(life)
     @height = life.size
     @width = life.first.size
-    @cells = Array.new(@height) { |y|
+    Array.new(@height) { |y|
       Array.new(@width) { |x|
         rect 100+W*x, 50+W*y, W, W, :fill => white
       }
     }
-    @initialized = true
   end
 
   def show_cells
     ## INSERT YOUR OWN CLASS WITH ITS ARGUMENTS HERE ##
-    game = GameOfLife.new IO.read('patterns/glider.txt')
-    @e = every 0.7 do |n|
-      @n.text = "Generation: #{n}"
-      life = game.evolve
-      @initialized ||= initialize_cells(life)
+    @game ||= GameOfLife.new IO.read('patterns/Gosper_glider_gun.txt')
+    @n ||= 0
+    @e = every @speed.text.to_f do
+      @generation.text = "Generation: #{@n += 1}"
+      life = @game.evolve
+      @cells ||= initialize_cells(life)
       @height.times{ |y|
         @width.times{ |x|
           @cells[y][x].style :fill => (life[y][x] == 0 ? white : green)
@@ -39,5 +39,8 @@ Shoes.app :title => 'Game of Life'do
   gb = stack
   para link('start') { gb.clear { show_cells } }
   para link('stop') { @e.stop }
-  @n = para
+  @generation = para
+  @speed = para '0.5'
+  para link('slower') { @e.stop; @speed.text = @speed.text.to_f+0.1; gb.clear { show_cells } }
+  para link('faster') { @e.stop; @speed.text = @speed.text.to_f-0.1; gb.clear { show_cells } }
 end
