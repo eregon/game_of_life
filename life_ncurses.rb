d8f1369b99cd3c3c63dcdb115994fd48b0909f50
eregon <eregontp@gmail.com>
2010-06-30 17:51:18 +0300

    Improved life_ncurses (fps, CL options), add patterns

--- life_ncurses.rb
+++ life_ncurses.rb
@@ -15,15 +15,15 @@ require 'rubygems'
 require 'ffi-ncurses'
 
 class LifeNcurses
-
-  # spaces from the border of the terminal
-  MARGIN = 1
+  MARGIN = 1 # spaces from the border of the terminal
   include FFI::NCurses
 
-  def initialize(game_of_life, iterations = 100)
+  def initialize(game_of_life, sleep = 0.05)
+    @sleep = sleep.to_f
     @stdscr = initscr
     cbreak
-    (1..iterations).each do |generation|
+    @last_time = Time.now
+    (1..1_000_000).each do |generation|
       clear
       display_title(generation)
       show game_of_life.evolve
@@ -36,15 +36,35 @@ class LifeNcurses
   def show(state)
     state.each_with_index do |row, row_index|
       row.each_with_index do |col, col_index|
-        mvwaddstr @stdscr, row_index+MARGIN, col_index+MARGIN, '#' if state[row_index][col_index] == 1
+        mvwaddstr @stdscr, MARGIN+row_index, MARGIN+col_index, '#' if state[row_index][col_index] == 1
       end
     end
     refresh
-    sleep 0.1
+    sleep @sleep
   end
 
   def display_title(generation)
-    mvwaddstr @stdscr, 0, 1, "Game of life: Generation #{generation}"
+    now = Time.now
+    mvwaddstr @stdscr, 0, 1, "Game of life: Generation #{generation}, fps: #{(1/(now-@last_time)).to_i}"
+    @last_time = now
   end
 
+end
+
+if __FILE__ == $0
+  require './lib/game_of_life'
+  DEFAULT_PATTERN = 'patterns/Gosper_glider_gun_static.txt'
+  DEFAULT_SLEEP = 0.05
+  if ARGV.delete '-h'
+    puts "ruby #{$0} [pattern=#{DEFAULT_PATTERN}] [sleep_time=#{DEFAULT_SLEEP}]"
+  else
+    LifeNcurses.new(
+      GameOfLife.new(
+        IO.read(
+          ARGV.shift || DEFAULT_PATTERN
+        )
+      ),
+      ARGV.shift || DEFAULT_SLEEP
+    )
+  end
 end
--- life_ncurses_script_example.rb
+++
@@ -1,6 +0,0 @@
-# require your game of life class
-require './lib/game_of_life'
-require './life_ncurses'
-
-# Use your own GameOfLife class, giving it the parameters you selected.
-LifeNcurses.new(GameOfLife.new(IO.read('patterns/Gosper_glider_gun.txt')))
