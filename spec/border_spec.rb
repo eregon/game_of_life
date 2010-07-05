910446cfe558aaa812d59fc4ccfd039965ca26ee
eregon <eregontp@gmail.com>
2010-07-05 13:54:41 +0300

    use normal spec as now the problem is found

--- spec/border_spec.rb
+++ spec/border_spec.rb
@@ -13,19 +13,10 @@ describe "GameOfLife (Border)" do
     it "#{spaceship} should be at the same place after a cycle" do
       spaceship, period, advance = send(spaceship)
       game = GameOfLife.new(spaceship.dup)
-      original_state = game.to_s
       cycle = (advance.last > advance.first ? spaceship.size : spaceship.first.size) *
               period / advance.max
       cycle.times { game.evolve }
-      new_state = game.to_s
-      if new_state == original_state
-        game.state.should == spaceship
-      else
-        # puts :original_state
-        # puts original_state
-        # puts :new_state
-        # puts new_state
-      end
+      game.state.should == spaceship
     end
   end
   
